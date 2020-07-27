975504868763ad84b4309bd5cede5382864cb5e5
Zachary Graham <zakgraham@gmail.com>
2020-07-26 17:12:37 -0700

    make 10 frames in template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playerCard: {
+        playerName: "",
+        playerCreated: false,
+      },
+      frames: Array(10).fill({ topRightScore: null, topLeftScore: null, bottomScore: null }),
+    };
+  }
+
+  // handleClick(i) {
+  //   const history = this.state.history.slice(0, this.state.stepNumber+1)
+  //   const current = history[history.length - 1];
+  //   const squares = current.squares.slice();
+  //   if (calculateWinner(squares) || squares[i]) {
+  //     return;
+  //   }
+  //   squares[i] = this.state.xIsNext ? 'X' : 'O';
+  //   this.setState({
+  //     history: history.concat([{
+  //       squares: squares
+  //     }]),
+  //     xIsNext: !this.state.xIsNext,
+  //     stepNumber: history.length,
+  //   });
+  // }
+
+  addToState = () => {
+    const currentFrames = this.state.frames.slice();
+    this.setState({
+      frames: currentFrames.concat([
+        {
+          topRightScore: 2,
+          topLeftScore: 3,
+          bottomScore: 1
+        },
+      ]),
+    });
+  };
+
+  render() {
+    const frames = this.state.frames.map((x) => {
+      return <div>{x.topRightScore}</div>;
+    });
+
+    return (
+      <div className="zg-app">
+        <div>bowling game</div>
+        <button onClick={this.addToState}>add</button>
+
+        <div>frames: {frames}</div>
+
+        <div className="zg-player-card">
+          <div className="zg-player-name">name</div>
+
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-frame">
+            <div className="zg-top-scores">
+              <div className="zg-top-left-score"></div>
+              <div className="zg-top-right-score"></div>
+            </div>
+            <div className="zg-bottom-score"></div>
+          </div>
+          <div className="zg-total-score">total score</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
